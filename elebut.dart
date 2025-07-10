353a26674574bee61649387446dc3a80e66313cb
Logi619 <logeshloge046@gmail.com>
2025-07-10 11:17:22 +0530

    Add files via upload

---
+++ elebut.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+class elebut extends StatefulWidget {
+  const elebut({super.key});
+
+  @override
+  State<elebut> createState() => _elebutState();
+}
+
+class _elebutState extends State<elebut> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Button"),
+      ),
+      body: ListView(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
---
+++ listil.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+class listil extends StatefulWidget {
+  final String title;
+  final String subtitle;
+  final String icon;
+  final String titlecolor;
+  final String iconcolor;
+
+  listil({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+    required this.titlecolor,
+    required this.iconcolor,
+
+
+
+
+});
+
+
+
+  @override
+  State<listil> createState() => _listilState();
+}
+
+class _listilState extends State<listil> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
